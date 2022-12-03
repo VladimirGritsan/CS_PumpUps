d0f1b4c41d442b67f718b8b490f678c79e8b9045
Vladimir-Gritsan <uni.former@mail.ru>
2022-12-03 13:50:35 +0300

    Initial commit

---
+++ Sorts/QuickSortDT.cs
@@ -0,0 +1,19 @@
+﻿namespace Algos.Sorts;
+
+public static class QuickSortDemoType
+{
+    public static void Demo()
+    {
+        int[] ints = { 3, -2, 7, 9, 5 };
+
+        void QuickSort(int begin, int end)
+        {
+            
+        }
+
+        (int i, int j, int pivot)? Partition()
+        {
+            return null;
+        }
+    }
+}
